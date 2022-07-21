ddc09088f8f7e3109a6d4fd32bf88d19ef9994b1
masfreddy93 <masieronicola@gmail.com>
2022-07-21 16:15:44 +0200

    add variables and click event in js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
-console.log('ciao')
-
-// creo gli elementi di input in html --> bottone, kmDaPercorrere, eta 
+// creo gli elementi di input in html --> bottone, kmDaPercorrere, eta --> ok
 // dichiaro la variabile kmDaPercorrere
+const kmDaPercorrere = document.querySelector('input[name="kmDaPercorrere"]');
 // dichiaro la variabile eta 
+const etaPasseggero = document.querySelector('input[name="kmDaPercorrere"]')
 // dichiaro la variabile bottone 
-// dichiaro la variabile prezzoAlKm
-//dichiaro la variabile prezzoBase 
+const buttonElement = document.querySelector('input[type="submit"]');
+console.log(buttonElement.value);
+
+//aggiungo l'evento 'click' al bottone; dichiaro prezzoBase e lo calcolo all'interno dell'evento
+
+buttonElement.addEventListener('click', function () {
+    const prezzoBase = parseFloat(kmDaPercorrere.value) * 0.21;
+    
+    console.log(prezzoBase);
+})
