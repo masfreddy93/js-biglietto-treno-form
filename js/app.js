54e6c4e132ddf868f8654082749b672b3163ca69
masfreddy93 <masieronicola@gmail.com>
2022-07-21 18:05:30 +0200

    js works

--- js/app.js
+++ js/app.js
@@ -1,56 +1,36 @@
 // creo gli elementi di input in html --> bottone, kmDaPercorrere, eta --> ok
 // dichiaro la variabile kmDaPercorrere
-const kmDaPercorrere = document.querySelector('input[name="kmDaPercorrere"]');
+let kmDaPercorrere = document.querySelector('input[name="kmDaPercorrere"]');
 // dichiaro la variabile eta 
 const etaPasseggero = document.querySelector('#etaPass')
-console.log(etaPasseggero)
 // dichiaro la variabile bottone 
 const buttonElement = document.querySelector('input[type="submit"]');
 console.log(buttonElement.value);
 
+
 //aggiungo l'evento 'click' al bottone; dichiaro prezzoBase e lo calcolo all'interno dell'evento
-
-// buttonElement.addEventListener('click', function () {
-//     const prezzoBase = parseFloat(kmDaPercorrere.value) * 0.21;
-    
-//     console.log(prezzoBase);
-// })
-
 //dichiaro variabile prezzoFinale e aggiugno condizioni età (esercizio di ieri)
-
-//lego opzioni del select a variabile js
-
-// const minorenne = document.querySelector('option[value="option1"]')
-// const maggiorenne = document.querySelector('option[value="option2"]')
-// const over65 = document.querySelector('option[value="option3"]')
-// console.log(minorenne.value, maggiorenne.value, over65.value)
-
 let prezzoFinale;
 
-// if (etaPasseggero == maggiorenne) {
-//     prezzoFinale = prezzoBase;
-//     } else if (etaPasseggero == minorenne) {
-//         prezzoFinale = prezzoBase * 0.8;
-//     } else {
-//         prezzoFinale = prezzoBase * 0.6;
-//     }
+buttonElement.addEventListener('click', function () {
 
+    const prezzoBase = parseFloat(kmDaPercorrere.value) * 0.21;
 
-    buttonElement.addEventListener('click', function () {
-        const prezzoBase = parseFloat(kmDaPercorrere.value) * 0.21;
-        
+    if (!isNaN(kmDaPercorrere.value) && (kmDaPercorrere.value >= 1)) {
         console.log(prezzoBase);
-        console.log(etaPasseggero.value)
+    
 
-        if (etaPasseggero.value == "option1") {
-            prezzoFinale = prezzoBase;
+            if (etaPasseggero.value == "option1") {
+            prezzoFinale = prezzoBase * 0.8;
             } else if (etaPasseggero.value == "option2") {
-                prezzoFinale = prezzoBase * 0.8;
+                prezzoFinale = prezzoBase;
+            } else if (etaPasseggero.value == "option0") {
+                alert('Seleziona una fascia d\'età valida');
             } else {
                 prezzoFinale = prezzoBase * 0.6;
-            }
-            console.log(prezzoFinale) 
-    })
-
-
-console.log(prezzoFinale) 
+           }
+            console.log(prezzoFinale);
+    } else {
+        console.log('Attenzione: Sono stati inseriti dei valori non corretti. Per favore, aggiornare la pagina e ripetere la procedura');
+    }
+})
