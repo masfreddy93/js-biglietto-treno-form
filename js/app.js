fa2fef745812a483ccbcd8c880494b6d250f928f
masfreddy93 <masieronicola@gmail.com>
2022-07-21 15:58:03 +0200

    add actions description in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 console.log('ciao')
+
+// creo gli elementi di input in html --> bottone, kmDaPercorrere, eta 
+// dichiaro la variabile kmDaPercorrere
+// dichiaro la variabile eta 
+// dichiaro la variabile bottone 
+// dichiaro la variabile prezzoAlKm
+//dichiaro la variabile prezzoBase 
